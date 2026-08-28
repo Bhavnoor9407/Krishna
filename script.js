cb695fcbee4fe1b00f737cc620bc24335cb4cf39
Bhavnoor Singh <ytzindabad08@gmail.com>
2026-08-28 05:40:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const noBtn = document.querySelector(".no-btn");
+const popup = document.querySelector("#popup");
+const closeBtn = document.querySelector("#closeBtn");
+const mainPage = document.querySelector("#mainPage");
+const yesBtn = document.querySelector(".yes-btn");
+const yesPopup = document.querySelector("#yespopup");
+const yesCloseBtn = document.querySelector("#yesCloseBtn");
+const finalPage = document.querySelector("#finalPage");
+
+
+noBtn.onclick = function() {
+    popup.style.display = "flex";
+};
+
+closeBtn.onclick = function() {
+    popup.style.display = "none";
+};
+
+
+yesBtn.onclick = function() {
+    yesPopup.style.display = "flex";
+};
+
+yesCloseBtn.onclick = function() {
+    yesPopup.style.display = "none";
+    mainPage.style.display = "none";
+    finalPage.style.display = "flex";
+};  
